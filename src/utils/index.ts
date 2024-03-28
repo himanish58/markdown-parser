e675a52f5cc0c8000d385abbc107905bd9a33473
Himanish Kabiraj <himanish.kabiraj@gmail.com>
2024-03-28 13:22:34 +0530

    fixed build errors

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,13 +9,13 @@ export const debounce = (
 	};
 };
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
 export const markdownToHtml = (markdown: string): string => {
 	// Regular expressions for basic Markdown syntax
 	const patterns = [
 		[
 			/^(#{1,6})\s(.*?)\n/gm,
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
 			(_, p1, p2) => `<h${p1.length}>${p2}</h${p1.length}>`,
 		], // Headers
 		[/\*\*(.*?)\*\*/g, '<strong>$1</strong>'], // Bold
@@ -31,11 +31,15 @@ export const markdownToHtml = (markdown: string): string => {
 		[/`(.*?)`/g, '<code>$1</code>'], // Inline code
 		[/```([^`]+)```/g, '<pre>$1</pre>'], // Code block
 		[/___+/g, '<hr />'], // Line Separator
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
 		[(/\n/g, '</p><p>')], // Paragraphs
 	];
 
 	// Apply each pattern to the markdown
 	for (const [pattern, replacement] of patterns) {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
 		markdown = markdown.replace(pattern, replacement);
 	}
 
